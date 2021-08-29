ed3acf9e3c6674fa453a28e706fdd30077a8ce54
daichi0412 <1410030157w@gmail.com>
2021-08-29 16:01:09 +0900

    user-management2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
-    validates :last_name, format: { with: /\A[一-龥ぁ-ん]/, message: "Last name is invalid.Input full-width characters." }
-    validates :first_name, format: { with: /\A[一-龥ぁ-ん]/, message: "First name is invalid.Input full-width characters" }
-    validates :pseudonym_last, format: { with: /\A[ァ-ヶー－]+\z/, message: "Last name kana is invalid.Input full-width katakana characters." }
-    validates :pseudonym_first, format: { with: /\A[ァ-ヶー－]+\z/, message: "First name kana is invalid.Input full-width katakana characters." }
+    validates :last_name, format: { with: /\A[一-龥ぁ-ん]/, message: "is invalid.Input full-width characters." }
+    validates :first_name, format: { with: /\A[一-龥ぁ-ん]/, message: "is invalid.Input full-width characters" }
+    validates :pseudonym_last, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid.Input full-width katakana characters." }
+    validates :pseudonym_first, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid.Input full-width katakana characters." }
     validates :birthday
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Include both letters and numbers." }
   end
