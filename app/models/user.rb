e72dd82f1760d00dddf57466366d737be38c31fe
daichi0412 <1410030157w@gmail.com>
2022-07-05 23:19:06 +0900

    reconfirmation2

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,13 @@ class User < ApplicationRecord
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid.Input full-width characters." }
     # ファーストネームの制約
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid.Input full-width characters" }
+    # ラストネーム（カナ）の制約
     validates :pseudonym_last, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid.Input full-width katakana characters." }
+    # ファーストネーム（カナ）の制約
     validates :pseudonym_first, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid.Input full-width katakana characters." }
+    # 誕生日の制約
     validates :birthday
+    # パスワードの制約
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Include both letters and numbers." }
   end
 end
